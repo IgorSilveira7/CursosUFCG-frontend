60f480066299104e136bf27e8237f7532656b6fd
Rickecr <richelton14@gmail.com>
2019-06-29 20:28:05 -0300

    Funcionalidade de dar like em um perfil

--- src/controller/PerfilController.js
+++ src/controller/PerfilController.js
@@ -4,6 +4,13 @@ import "../components/Comentarios/LstComentarios.js";
 
 let Perfil = {};
 
+// botões
+const btnLike = document.getElementById("likeBtn");
+const btnComment = document.getElementById("commentBtn");
+
+btnLike.onclick = () => like();
+btnComment.onclick = () => comentar();
+
 const idPerfil = getPerfil() || 1;
 let ps = document.getElementById("ps-comentarios");
 ps.setAttribute("id_perfil", idPerfil);
@@ -39,6 +46,10 @@ async function api() {
 
 async function render() {
     await api();
+    if (Perfil.usuarioAutenticadoCurtiu) {
+        btnLike.style.color = "red";
+    }
+
     const $id = document.getElementById("id");
     const $nome = document.getElementById("nome");
     const $like = document.getElementById("like");
@@ -48,6 +59,38 @@ async function render() {
     $nome.innerText = "Nome: " + Perfil.disciplina.nome;
     $like.innerText = "Curtidas: " + Perfil.curtidas.length;
     $media.innerText = "Média: " + Perfil.media;
+}
+
+async function like() {
+    let headers = {
+        'Access-Control-Allow-Origin': '*',
+        'Content-Type': 'application/json',
+    };
+
+    let config = {
+        method: 'POST',
+        headers:  headers,
+        mode: "cors"
+    };
+
+    try {
+        const url = BASEURL + "/v1/perfil/curtir/" + idPerfil + "/" + getEmail();
+        let response = await fetch(url, config);
+        if (!response.ok) {
+            throw response;
+        }
+
+        let perfil = await response.json();
+        Perfil = perfil;
+        console.log(Perfil);
+    } catch (error) {
+        let e = await error.json();
+        console.log(e);
+    }
+}
+
+async function comentar() {
+    
 }
 
 render();
