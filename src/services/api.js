725fcdf4b9bd58ba821b3caccc9b637c972a8d7a
Rich Elton <richelton14@gmail.com>
2019-07-03 15:10:51 -0300

    Corrigindo BUG que eu tenha deixado

--- src/services/api.js
+++ src/services/api.js
@@ -1,13 +1,3 @@
 const BASEURL = "https://api-cursosufcg.herokuapp.com/api";
 
-const api = (url, config) => {
-    let repsonse = await  fetch(url, config);
-    if (!response.ok) {
-        throw response;
-    }
-    let data = await response.json();
-
-    return data;
-}
-
-export { BASEURL, api };
+export { BASEURL };
